38eec7956871ba2258ca639f547323712f63fa67
FaultyPine <gclark1013@gmail.com>
2025-01-20 23:11:44 -0800

    beginning to think about data structures, beginning concept of merging data layouts

--- binmerge.cpp
+++ binmerge.cpp
@@ -4,7 +4,10 @@
 #include <cstdlib>
 #include <cstring>
 
+#include <set>
+
 #include "type_enumeration.h"
+
 
 struct FieldData
 {
@@ -13,24 +16,52 @@ struct FieldData
     // But honestly i think an enum and a switch case with printf specifiers is fine
     Type type;
     char* data = 0;
-    char name[40] = {0};
+    #define MAX_IDENTIFIER_LENGTH (2048)
+    char name[MAX_IDENTIFIER_LENGTH] = {0};
 };
+bool AreFieldsSame(const FieldData* first, const FieldData* second)
+{
+    return first->size == second->size && first->type == second->type && strncmp(first->name, second->name, MAX_IDENTIFIER_LENGTH) == 0;
+}
 
 struct FormatLayout
 {
     uint32_t magic = 0;
     size_t fieldsCount = 0;
     FieldData fields[];
-    size_t GetStructureSize() const
-    {
-        size_t result = 0;
-        for (size_t i = 0; i < fieldsCount; i++)
-        {
-            result += fields[i].size;
-        }
-        return result;
-    }
 };
+size_t GetStructureSize(FormatLayout* layout)
+{
+    size_t result = 0;
+    for (size_t i = 0; i < layout->fieldsCount; i++)
+    {
+        result += layout->fields[i].size;
+    }
+    return result;
+}
+const FieldData* DoesFormatHaveField(const FormatLayout* layout, const FieldData* field)
+{
+    FieldData* result = nullptr;
+    for (int i = 0; i < layout->fieldsCount; i++)
+    {
+        if (AreFieldsSame(&layout->fields[i], field))
+        {
+            return &layout->fields[i];
+        }
+    }
+    return result;
+}
+void PrintMe(FormatLayout* layout)
+{
+    printf("magic: %u", layout->magic);
+    printf("num fields: %ull", layout->fieldsCount);
+    for (int i = 0; i < layout->fieldsCount; i++)
+    {
+        printf("field: %s\n", layout->fields[i].name);
+        printf("size: %ull", layout->fields[i].size);
+        printf("data as str: %.*s\n", layout->fields[i].size, layout->fields[i].data);
+    }
+}
 
 // -----------------------------
 // EXAMPLE HARDCODED TYPE
@@ -45,15 +76,16 @@ struct ExampleFileFormat
     Vector3 pos;
     char name[20];
     uint64_t counter;
-    void PrintMe()
-    {
-        printf("ExampleFileFormat:");
-        printf("x = %i\n", x);
-        printf("pos = %f %f %f\n", pos.x, pos.y, pos.z);
-        printf("name = %s\n", name);
-        printf("counter = %lu\n", counter);
-    }
 };
+void PrintMe(ExampleFileFormat* fileformat)
+{
+    printf("ExampleFileFormat:");
+    printf("x = %i\n", fileformat->x);
+    printf("pos = %f %f %f\n", fileformat->pos.x, fileformat->pos.y, fileformat->pos.z);
+    printf("name = %s\n", fileformat->name);
+    printf("counter = %lu\n", fileformat->counter);
+}
+
 static FormatLayout ExampleFileFormatHardcodedMetadata =
 {
     .magic = 0xDEADBEEF,
@@ -120,7 +152,18 @@ bool AtomicMergeModificationResult(const FieldData& base, const FieldData& local
     return true;
 }
 
-FormatLayout MergeFormats(const FormatLayout& base, const FormatLayout& local, const FormatLayout& remote)
+
+// when merging, we require 6 pieces of info
+// base revision, local revision and remote revision
+// each needing the file format layout metadata, and the actual file contents
+// TODO: the "ExampleFileFormat" is temp for testing and iterating. Make it generic in the future
+FormatLayout MergeFormats(
+    const FormatLayout& base, 
+    const FormatLayout& local, 
+    const FormatLayout& remote,
+    const ExampleFileFormat& fileBase,
+    const ExampleFileFormat& fileLocal,
+    const ExampleFileFormat& fileRemote)
 {
     // we never expect the magic to change. 
     auto magic = base.magic;
@@ -132,30 +175,58 @@ FormatLayout MergeFormats(const FormatLayout& base, const FormatLayout& local, c
         return {};
     }
     FormatLayout mergedResult = {0};
-    // how to merge arbitrary potentially recursively complex structures...
-    for (size_t i = 0; i < base.fieldsCount; i++)
+    // here is the meat. Merging arbitrary structures...
+
+    // a single "instance" of a field reordering in some revision
+    // "foreign" means not-my-revision. So in terms of local changes, the foreign changes are remote and vise-versa
+    // since we do diff checking from the "perspective" of some revision, that revision is called "native"
+    struct Reorder
     {
+        uint32_t nativeFieldIndex = UINT32_MAX; // the index of "our own" field
+        uint32_t foreignFieldIndex = UINT32_MAX; // the index of the "foreign" field that was once *this* field, but has now been reordered
+    };
+    // 1 of these per "revision" I.E. local/remote changes
+    // EX: unique added fields in local changes
+    struct RevisionData
+    {
+        std::set<FieldData*> addedFields = {};
+        std::set<FieldData*> removedFields = {};
+        std::set<Reorder> reoderedFields = {};
+    };
+    struct MergeScratchpad
+    {
+        RevisionData localData = {};
+        RevisionData remoteData = {};
+    };
+    // first, from the perspective of the local changes
+    // General idea: look at changes from the perspective of local, against remote, using a base.
+    // the vise-versa, from the perspective of remote, against local, using the same base.
+    const FormatLayout& nativeLayout = local;
+    const FormatLayout& foreignLayout = remote;
+    for (size_t i = 0; i < nativeLayout.fieldsCount; i++)
+    {
+        const FieldData& baseField = base.fields[i];
+        const FieldData& nativeField = nativeLayout.fields[i];
+        const FieldData& foreignField = foreignLayout.fields[i];
+        FieldData& mergedResultField = mergedResult.fields[i];
+
         // need to handle a few cases
         
         // first, case where all fields match up on all revisions
         // in that case, can just iterate through fields and check for modifications, using AtomicMergeModificationResult
 
-        // next, case where field(s) have been added
+        //=========== next, case where field(s) have been added
+        // does this field exist in both local and remote? if so, it has not been added and noop
+        // if it exists in one, and not the other, add it to the relevant revision's added fields list
+
+        const FieldData* isForeignFieldInNativeLayout = DoesFormatHaveField(&nativeLayout, &foreignField);
+
+
+        // ==========
 
         // next, case where field(s) have been removed
 
         // next, case where field(s) have been re-ordered
-        
-        const auto& baseField = base.fields[i];
-        const auto& localField = local.fields[i];
-        const auto& remoteField = remote.fields[i];
-        auto& mergedResultField = mergedResult.fields[i];
-        //Types type = formatMetadata.fields[i].type;
-        // if (!AtomicMergeResult(baseField, localField, remoteField, mergedResultField)) 
-        // { 
-        //     printf("merge conflict! %s\n", base.fields[i].name); 
-        //     __debugbreak(); 
-        // } 
     }
     return mergedResult;
 }
@@ -188,9 +259,13 @@ int main(int argc, char* argv[])
         .counter = 123
     };
 
-    FormatLayout merged = MergeFormats(ExampleFileFormatHardcodedMetadata, &base, &local, &remote);
+    FormatLayout merged = MergeFormats(
+        ExampleFileFormatHardcodedMetadata, 
+        ExampleFileFormatHardcodedMetadata,
+        ExampleFileFormatHardcodedMetadata,
+        base, local, remote);
     printf("Resulting merged data:\n");
-    merged->PrintMe();
+    PrintMe(&merged);
     
     return 0;
 }
